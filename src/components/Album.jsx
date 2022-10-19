fce626d7d70955b8f6e353ab8bb674065369ae0d
bbluechip <cs.orhan.berk@gmail.com>
2022-10-19 18:07:21 +0200

    added button toggle

---
+++ src/components/Album.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./album.scss";
+const Album = () => {
+  return <div className="albumDiv">Album</div>;
+};
+
+export default Album;
---
+++ src/components/Main.jsx
@@ -0,0 +1,20 @@
+import Album from "./Album";
+import { useState } from "react";
+import "./main.scss";
+const Button = () => {
+  const [show, setShow] = useState(true);
+  const showHandler = () => {
+    setShow(!show);
+  };
+  return (
+    <div className="mainDiv">
+      <h1>ALBUMS</h1>
+      <button className="button2" onClick={showHandler}>
+        SHOW / HIDE
+      </button>
+      {show ? <Album /> : <></>}
+    </div>
+  );
+};
+
+export default Button;
